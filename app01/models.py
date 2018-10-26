96018044790b51fd590e637005bd976c42d9a60f
dewlytg <dewly_tg@163.com>
2018-10-26 11:16:42 +0800

    正向反向查询

--- app01/models.py
+++ app01/models.py
@@ -46,3 +46,43 @@ class Publisher(models.Model):
 class Upload(models.Model):
     path = models.CharField(max_length=128)
 
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32,unique=True)
+    email = models.EmailField(max_length=32,unique=True)
+    pwd = models.CharField(max_length=128)
+    ctime = models.DateTimeField(auto_now_add=True)
+    user_type = models.ForeignKey(to="UserType",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+    def save(self,*args,**kwargs):
+        self.pwd = hashlib.md5(self.pwd.encode()).hexdigest()
+        super(User,self).save(*args,**kwargs)
+
+
+class UserType(models.Model):
+    caption = models.CharField(max_length=16,unique=True)
+    menus = models.ManyToManyField("Menu")
+
+    def __str__(self):
+        return self.caption
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=32,unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=32,unique=True)
+    url_path = models.CharField(max_length=64,unique=True)
+    menu = models.ForeignKey("Menu",on_delete=models.CASCADE,related_name="op")
+
+    def __str__(self):
+        return self.name
+
